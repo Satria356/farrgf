fdf41f9ea6bd13428993789a453f04db1ea84f3f
Satria356 <102003881+Satria356@users.noreply.github.com>
2022-05-04 13:36:33 +0700

    Add files via upload

--- connect/config.js
+++ connect/config.js
@@ -11,17 +11,17 @@ const chalk = require("chalk")
 global.thum = fs.readFileSync("./worker/media/image/thumb.jpg")
 global.log0 = fs.readFileSync("./worker/media/image/thumb.jpg")
 global.err4r = fs.readFileSync("./worker/media/image/thumb.jpg")
-global.owner = ['6283811034750','2','6283163410880']
+global.owner = ['6283811034750','6283163410880','6283168004413']
 global.rkyt = []
 global.banUser = ['6281366679021@s.whatsapp.net']
 global.ntilink = ['120363043965529911@g.us','120363022988111568@g.us']
 global.wlcm = ['120363043965529911@g.us','120363023112413670@g.us','19054091487-1424650263@g.us','120363022988111568@g.us']
 global.gcrevoke = ['19054091487-1424650263@g.us']
-global.packname = "Fahri"
-global.packname2 = "BY Fara ~ Farbot"
-global.author = "Fara"
+global.packname = "FARBOT"
+global.packname2 = "By Fara"
+global.author = "FARA CHAN"
 global.sessionName = "session"
-global.linkgrupss = "https://chat.whatsapp.com/HqALA51aiURFp4O3BfWlw4"
+global.linkgrupss = "https://chat.whatsapp.com/J0GieU5rJLlHqSBQaKqeqF"
 global.antitags = true
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.mess = {
@@ -32,7 +32,7 @@ global.mess = {
     group: 'Fitur Digunakan Hanya Untuk Group!',
     private: 'Fitur Digunakan Hanya Untuk Private Chat!',
     bot: 'Fitur Khusus Pengguna Nomor Bot',
-    wait: 'Processing data.....',
+    wait: 'Proses....',
     linkm: 'Linknya Mana Kak?',
     ban: 'Kamu telah dibanned oleh owner, Jika ingin di unbanned chat owner'
 }
